55f8c3cb2d058628b830bf89bb909272e25815b3
Nekle <nekle@NekledeMacBook-Air.local>
2014-07-10 13:18:26 +0800

    add error handler

--- server/controllers/news.js
+++ server/controllers/news.js
@@ -15,45 +15,67 @@ exports.io = function (socket) {
     var pageSize = 10;
     socket.on('news.list', function (data) {
         News.find().skip(pageSize * data.pageId).limit(pageSize).exec(function (err, docs) {
-            socket.emit('news.list', docs);
-            console.log('news.list');
+            var payload = { action: false, info:'', data:'' };
+            if (err) {
+                payload.info = err;
+            } else {
+                payload.action = true;
+                payload.data = docs;
+            }
+            socket.emit('news.list', payload);
         });
     });
 
     socket.on('news.item', function (data) {
         News.findById(data.id).exec(function (err, doc) {
-            socket.emit('news.item', { action:true, info:'OK', data:doc });
+            var payload = { action: false, info:'', data:'' };
+            if (err) {
+                payload.info = err;
+            } else {
+                payload.action = true;
+                payload.data = doc;
+            }
+            socket.emit('news.item', payload);
         });
     });
 
     socket.on('news.add', function (data) {
         var news = new News(data);
         news.save(function (err, doc) {
+            var payload = { action: false, info:'', data:'' };
             if (err) {
-                socket.emit('news.add', { action:false, info:err });
-                return ;
+                payload.info = err;
+            } else {
+                payload.action = true;
+                payload.data = doc;
             }
-            socket.emit('news.add', { action:true, info:'OK', data:doc });
+            socket.emit('news.add', payload);
         });
     });
 
     socket.on('news.update', function (data) {
         News.findByIdAndUpdate(data._id, { title:data.title, url:data.url }, function (err, doc) {
+            var payload = { action: false, info:'', data:'' };
             if (err) {
-                socket.emit('news.update', { action:false, info:err });
-                return ;
+                payload.info = err;
+            } else {
+                payload.action = true;
+                payload.data = doc;
             }
-            socket.emit('news.update', { action:true, info:'OK', data:doc });
+            socket.emit('news.update', payload);
         });
     })
 
     socket.on('news.delete', function (data) {
         News.findByIdAndRemove(data._id).exec(function (err, doc) {
+            var payload = { action: false, info:'', data:'' };
             if (err) {
-                socket.emit('news.delete', { action:false, info:err });
-                return ;
+                payload.info = err;
+            } else {
+                payload.action = true;
+                payload.data = doc;
             }
-            socket.emit('news.delete', { action:true, info:'OK', data:doc });
+            socket.emit('news.delete', payload);
         });
     });
 }
