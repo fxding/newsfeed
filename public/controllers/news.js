18fda15dd7ef7cdb4bcaaa931d06f3b0b12171f2
Nekle <nekle@NekledeMacBook-Air.local>
2014-07-10 13:58:03 +0800

    wrap angular in (function () {  ...  })();

--- public/controllers/news.js
+++ public/controllers/news.js
@@ -1,79 +1,81 @@
-angular.module('News').controller('NewsController', ['$scope', 'Socket', function ($scope, Socket) {
-    $scope.newsList = []; // store all news items
-    $scope.newsItem = {};
-    $scope.pageId = 0; // current page NO.
+(function () {
+    angular.module('News').controller('NewsController', ['$scope', 'Socket', function ($scope, Socket) {
+        $scope.newsList = []; // store all news items
+        $scope.newsItem = {};
+        $scope.pageId = 0; // current page NO.
 
-    $scope.nextPage = function (pageId) {
-        console.log(pageId);
-        // request next page
-        Socket.emit('news.list', { pageId: pageId});
-    };
+        $scope.nextPage = function (pageId) {
+            console.log(pageId);
+            // request next page
+            Socket.emit('news.list', { pageId: pageId});
+        };
 
-    Socket.on('news.list', function (data) {
-        if (data.action === false) {
-            console.log(data);
-            return;
+        Socket.on('news.list', function (data) {
+            if (data.action === false) {
+                console.log(data);
+                return;
+            }
+            $scope.newsList = data.data;
+            $scope.pageId += 1;
+        });
+
+        // request first page
+        $scope.nextPage($scope.pageId);
+    }])
+    .controller('EditController', ['$scope', '$location', '$routeParams', 'Socket', function ($scope, $location, $routeParams, Socket) {
+        var id = $routeParams.id;
+
+        // Get news item by id
+        var getNewsItem = function (id) {
+            Socket.emit('news.item', {id:id});
         }
-        $scope.newsList = data.data;
-        $scope.pageId += 1;
-    });
-
-    // request first page
-    $scope.nextPage($scope.pageId);
-}])
-.controller('EditController', ['$scope', '$location', '$routeParams', 'Socket', function ($scope, $location, $routeParams, Socket) {
-    var id = $routeParams.id;
-
-    // Get news item by id
-    var getNewsItem = function (id) {
-        Socket.emit('news.item', {id:id});
-    }
-    Socket.on('news.item', function (data) {
-        if (data.action === false) {
+        Socket.on('news.item', function (data) {
+            if (data.action === false) {
+                console.log(data);
+                return;
+            }
             console.log(data);
-            return;
-        }
-        console.log(data);
-        $scope.newsItem = data.data;
-    });
+            $scope.newsItem = data.data;
+        });
 
-    // update action
-    $scope.update = function (data) {
-        Socket.emit('news.update', data);
-    }
-    Socket.on('news.update', function (data) {
-        if (data.action === false) {
-            console.log(data);
-            return;
+        // update action
+        $scope.update = function (data) {
+            Socket.emit('news.update', data);
         }
-        $location.path('/');
-    });
+        Socket.on('news.update', function (data) {
+            if (data.action === false) {
+                console.log(data);
+                return;
+            }
+            $location.path('/');
+        });
 
-    // delete action
-    $scope.delete = function (data) {
-        Socket.emit('news.delete', data);
-    }
-    Socket.on('news.delete', function (data) {
-        if (data.action === false) {
-            console.log(data);
-            return;
+        // delete action
+        $scope.delete = function (data) {
+            Socket.emit('news.delete', data);
         }
-        $location.path('/');
-    });
+        Socket.on('news.delete', function (data) {
+            if (data.action === false) {
+                console.log(data);
+                return;
+            }
+            $location.path('/');
+        });
 
-    // Get item by id
-    getNewsItem(id);
-}])
-.controller('AddController', ['$scope', '$location', 'Socket', function ($scope, $location, Socket) {
-    // add action
-    $scope.add = function (data) {
-        Socket.emit('news.add', data);
-    }
-    Socket.on('news.add', function (data) {
-        if (data.action === false) {
-            console.log(data);
-            return;
+        // Get item by id
+        getNewsItem(id);
+    }])
+    .controller('AddController', ['$scope', '$location', 'Socket', function ($scope, $location, Socket) {
+        // add action
+        $scope.add = function (data) {
+            Socket.emit('news.add', data);
         }
-        $location.path('/');
-    });
-}]);
+        Socket.on('news.add', function (data) {
+            if (data.action === false) {
+                console.log(data);
+                return;
+            }
+            $location.path('/');
+        });
+    }]);
+})();
